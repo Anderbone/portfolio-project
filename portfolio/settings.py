9eb96348d2919d38f9e491be319e49d1aa20273d
Anderbone <yanchiyu@outlook.com>
2019-06-28 21:03:43 +0000

    tweask remote

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -134,6 +134,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'  # only for web url here
 
 try:
-    from local_settings import *
+    from .local_settings import *
 except ImportError:
     pass
