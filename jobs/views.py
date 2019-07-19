bbb54891789e74a5ae84ade89e4fc101bfaeb947
Anderbone <yanchiyu@outlook.com>
2019-07-20 00:43:45 +0100

    blog instead of jobs

--- jobs/views.py
+++ jobs/views.py
@@ -5,11 +5,11 @@ from ..blog.models import Blog
 
 # Create your views here.
 
-# def home(request):
-#     jobs = Job.objects
-#     return render(request, 'jobs/home.html', {'jobs':jobs})
-
 def home(request):
-    blogs = Blog.objects
     jobs = Job.objects
-    return render(request, 'jobs/home.html', {'blogs':blogs})
+    return render(request, 'jobs/home.html', {'jobs':jobs})
+
+# def home(request):
+#     blogs = Blog.objects
+#     jobs = Job.objects
+#     return render(request, 'jobs/home.html', {'blogs':blogs})
