da58494fbd35c5126a536f009cc752e3d79e01d5
Anderbone <yanchiyu@outlook.com>
2019-07-19 23:54:44 +0100

    test

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,7 @@ class Blog(models.Model):
     title = models.CharField(max_length=255)
     pub_date = models.DateTimeField(null=True, blank=True)
     body = models.TextField()
+    test2 = models.CharField()
     summary = models.TextField()
     # image = models.ImageField(upload_to='images/')
 
