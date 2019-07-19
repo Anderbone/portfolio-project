6a51ee4451f59165a00522e436c12fb2879529f2
Anderbone <yanchiyu@outlook.com>
2019-07-20 00:41:35 +0100

    blog instead of jobs

--- jobs/views.py
+++ jobs/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 
 from .models import Job
+from ..blog.models import Blog
 
 # Create your views here.
 
+# def home(request):
+#     jobs = Job.objects
+#     return render(request, 'jobs/home.html', {'jobs':jobs})
+
 def home(request):
+    blogs = Blog.objects
     jobs = Job.objects
-    return render(request, 'jobs/home.html', {'jobs':jobs})
+    return render(request, 'jobs/home.html', {'blogs':blogs})
