91de88782f2f34beaf0a2eede66cb387f9836d23
Anderbone <yanchiyu@outlook.com>
2019-07-19 23:43:24 +0100

    remove pic, add summary

--- blog/models.py
+++ blog/models.py
@@ -5,13 +5,14 @@ class Blog(models.Model):
     title = models.CharField(max_length=255)
     pub_date = models.DateTimeField(null=True, blank=True)
     body = models.TextField()
-    image = models.ImageField(upload_to='images/')
+    summary = models.TextField()
+    # image = models.ImageField(upload_to='images/')
 
     def __str__(self):
         return self.title
 
     def summary(self):
-        return self.body[:50]
+        return self.summary
 
     def pub_date_pretty(self):
         return self.pub_date.strftime('%b %e %Y')
