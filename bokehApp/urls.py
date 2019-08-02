1531d16b25e985090a62d4b0de70596ca01622db
Anderbone <yanchiyu@outlook.com>
2019-08-03 00:30:16 +0100

    graph view.py add all from github ytb all

--- bokehApp/urls.py
+++ bokehApp/urls.py
@@ -9,4 +9,12 @@ urlpatterns = [
     # path('', views.webcv ,name = 'webcv')
     # path('', )
     path('', views.graph, name='graph'),
+
+    path("starter/", views.starter, name="starter"),
+    path("combo/", views.combo, name="combo"),
+    path("programming/", views.programming, name="programming"),
+    path("multi_plot/", views.multi_plot, name="multi_plot"),
+    path("products/", views.products, name="products"),
+    path("pie/", views.pie, name="pie"),
+    path("test_html/", views.test_html, name="test_html"),
 ]
