ce863616d473c369ce3d75ff75bc88b6aa1264e7
Anderbone <yanchiyu@outlook.com>
2019-08-03 23:01:51 +0100

    big change for d2

--- bokehApp/views.py
+++ bokehApp/views.py
@@ -6,6 +6,7 @@ from django.http import HttpResponse
 
 
 # Create your views here.
+import numpy as np
 import sys
 sys.path.append('../')
 
@@ -26,6 +27,10 @@ from bokeh.layouts import gridplot
 from bokeh.palettes import Viridis3
 from bokeh.embed import json_item
 from bokeh.sampledata.iris import flowers
+
+from bokeh.layouts import column
+from bokeh.models import ColumnDataSource, RangeTool
+from bokeh.sampledata.stocks import AAPL
 
 def graph0(request):
     plot = figure()
@@ -78,8 +83,32 @@ def graph(request):
     # p3 = figure(plot_width=250, plot_height=250, title=None)
     # p3.square(x, y, size=10, color=Viridis3[2])
 
-    p2 = figure()
-    p2.circle([1, 10, 35, 27], [0, 0, 0, 0], size=20, color="blue")
+    dates = np.array(AAPL['date'], dtype=np.datetime64)
+    source = ColumnDataSource(data=dict(date=dates, close=AAPL['adj_close']))
+
+    p = figure(plot_height=300, plot_width=800, tools="xpan", toolbar_location=None,
+               x_axis_type="datetime", x_axis_location="above",
+               background_fill_color="#efefef", x_range=(dates[1500], dates[2500]))
+
+    p.line('date', 'close', source=source)
+    p.yaxis.axis_label = 'Price'
+
+    select = figure(title="Drag the middle and edges of the selection box to change the range above",
+                    plot_height=130, plot_width=800, y_range=p.y_range,
+                    x_axis_type="datetime", y_axis_type=None,
+                    tools="", toolbar_location=None, background_fill_color="#efefef")
+
+    range_tool = RangeTool(x_range=p.x_range)
+    range_tool.overlay.fill_color = "navy"
+    range_tool.overlay.fill_alpha = 0.2
+
+    select.line('date', 'close', source=source)
+    select.ygrid.grid_line_color = None
+    select.add_tools(range_tool)
+    select.toolbar.active_multi = range_tool
+
+    # show(column(p, select))
+
     p3 = figure()
     p3.circle([1, 10, 35, 27], [0, 0, 0, 0], size=20, color="red")
 
@@ -87,7 +116,7 @@ def graph(request):
     # grid = gridplot([[plot, p2], [None, p3]])
     # Store components
     # script, div = components(plot)
-    script, (p1div, p2div, p3div) = components((plot, p2, p3))
+    script, (p1div, p2div, p3div) = components((plot, column(p,select), p3))
     # script, div = components(p3)
     # return render(request, 'graph.html', {'script': script, 'div': div})
     return render(request, 'graph.html', {'script': script, 'div1': p1div, 'div2':p2div, 'div3':p3div})
