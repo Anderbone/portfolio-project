4494933caa4799523d02e0b4cfa19a574deeb0ae
Anderbone <yanchiyu@outlook.com>
2019-08-04 15:33:30 +0100

    p0

--- bokehApp/urls.py
+++ bokehApp/urls.py
@@ -10,11 +10,11 @@ urlpatterns = [
     # path('', )
     path('', views.graph, name='graph'),
 
-    path("starter/", views.starter, name="starter"),
-    path("combo/", views.combo, name="combo"),
-    path("programming/", views.programming, name="programming"),
-    path("multi_plot/", views.multi_plot, name="multi_plot"),
-    path("products/", views.products, name="products"),
-    path("pie/", views.pie, name="pie"),
-    path("test_html/", views.test_html, name="test_html"),
+    # path("starter/", views.starter, name="starter"),
+    # path("combo/", views.combo, name="combo"),
+    # path("programming/", views.programming, name="programming"),
+    # path("multi_plot/", views.multi_plot, name="multi_plot"),
+    # path("products/", views.products, name="products"),
+    # path("pie/", views.pie, name="pie"),
+    # path("test_html/", views.test_html, name="test_html"),
 ]
